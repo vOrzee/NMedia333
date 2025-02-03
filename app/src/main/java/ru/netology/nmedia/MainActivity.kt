e0a71b47afe0f52f2c27f264043be5b4aa29ac64
ilya_ <ilya_prokopenko@mail.ru>
2025-02-04 01:01:04 +0300

    RecyclerView in progress #1

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.databinding.CardPostBinding
+import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -16,38 +18,47 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        applyInset(binding.main)
+        applyInset(binding.root)
 
         val viewModel: PostViewModel by viewModels()
 
-        viewModel.data.observe(this) { post ->
-            with(binding) {
-                author.text = post.author
-                content.text = post.content
-                published.text = post.published
-
-                like.setImageResource(
-                    if (post.likedByMe) {
-                        R.drawable.ic_liked_24
-                    } else {
-                        R.drawable.ic_like_24
-                    }
-                )
-
-                likeCount.text = CountCalculator.calculate(post.likes)
-
-                shareCount.text = CountCalculator.calculate(post.shares)
-
-                viewsCount.text = CountCalculator.calculate(post.views)
+        viewModel.data.observe(this) { posts ->
+            binding.main.removeAllViews()
+            posts.forEach { post ->
+                val cardPostBinding = CardPostBinding.inflate(layoutInflater, binding.main, true)
+                bindPost(cardPostBinding, post)
+                cardPostBinding.like.setOnClickListener {
+                    viewModel.likeById(post.id)
+                }
+                cardPostBinding.share.setOnClickListener {
+                    viewModel.shareById(post.id)
+                }
             }
         }
+    }
 
-        binding.like.setOnClickListener {
-            viewModel.like()
-        }
+    private fun bindPost(
+        binding: CardPostBinding,
+        post: Post
+    ) {
+        with(binding) {
+            author.text = post.author
+            content.text = post.content
+            published.text = post.published
 
-        binding.share.setOnClickListener {
-            viewModel.share()
+            like.setImageResource(
+                if (post.likedByMe) {
+                    R.drawable.ic_liked_24
+                } else {
+                    R.drawable.ic_like_24
+                }
+            )
+
+            likeCount.text = CountCalculator.calculate(post.likes)
+
+            shareCount.text = CountCalculator.calculate(post.shares)
+
+            viewsCount.text = CountCalculator.calculate(post.views)
         }
     }
 
--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -9,13 +9,13 @@ import ru.netology.nmedia.repository.PostRepositoryInMemory
 class PostViewModel : ViewModel() {
     private val repository: PostRepository = PostRepositoryInMemory()
 
-    val data: LiveData<Post> = repository.get()
+    val data: LiveData<List<Post>> = repository.get()
 
-    fun like() {
-        repository.like()
+    fun likeById(id: Long) {
+        repository.likeById(id)
     }
 
-    fun share() {
-        repository.share()
+    fun shareById(id: Long) {
+        repository.shareById(id)
     }
 }
